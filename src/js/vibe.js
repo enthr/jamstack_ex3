808a9683d6ce0ee4c092a682ccc50aa661a06354
Jaimin Prajapati <enthralall@gmail.com>
2021-05-29 11:37:30 +0530

    initial commit

---
+++ src/js/vibe.js
@@ -0,0 +1,5 @@
+const vibes = ["and you are awesome", "have a wonderful day", "and you've get this", "and so is this puppy"];
+
+let vibe = vibes[Math.floor(Math.random() * Math.floor(vibes.length))];
+
+document.querySelector('.vibe').append(vibe);
